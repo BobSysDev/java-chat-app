b3d271480161090d64d4f6c02cf9b95e9e198545
Marius Gafton <mariusgafton2005@gmail.com>
2024-03-18 10:30:44 +0100

    ChatModel, ChatModelManager, Utility

--- ShatAppServer/src/model/Message.java
+++ ShatAppServer/src/model/Message.java
@@ -2,4 +2,46 @@ package model;
 
 public class Message
 {
+    private String message;
+    private String sender;
+    private String timestamp;
+
+    public Message(String message, String sender, String timestamp)
+    {
+        this.message = message;
+        this.sender = sender;
+        this.timestamp = timestamp;
+    }
+
+    public Message(String message, String sender)
+    {
+        this.message = message;
+        this.sender = sender;
+        this.timestamp = null;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public String getSender(){
+        return sender;
+    }
+
+   public String getTimestamp(){
+        return timestamp;
+   }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+
+    @Override public String toString(){
+        return "["
+    }
 }
