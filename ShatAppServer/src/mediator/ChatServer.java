a701970589c6c2ec88379b152808c91ee657fd0d
Marius Gafton <mariusgafton2005@gmail.com>
2024-03-18 12:47:09 +0100

    ChatServer class

--- ShatAppServer/src/mediator/ChatServer.java
+++ ShatAppServer/src/mediator/ChatServer.java
@@ -1,4 +1,36 @@
 package mediator;
 
-public class ChatServer {
+import model.ChatModel;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer
+{
+    private ServerSocket chatSocket;
+    private ChatModel chatModel;
+
+    public ChatServer(ChatModel chatModel, int port) throws IOException
+    {
+        this.chatModel = chatModel;
+        this.chatSocket = new ServerSocket(port);
+        execute();
+    }
+
+    private void execute(){
+        System.out.println("Starting server...");
+        while(true){
+            System.out.println("Waiting for a client...");
+            Socket socket = null;
+            try{
+                socket = chatSocket.accept();
+                Thread clientThread = new Thread(new ChatClientHandler(socket,chatModel));
+                clientThread.start();
+            }
+            catch (Exception e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
