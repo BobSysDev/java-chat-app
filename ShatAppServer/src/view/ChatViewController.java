cc3b0386b6e9cd39c68bf47f730bf04aca95fc4f
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2024-03-19 19:45:27 +0100

    ViewControllers base

--- ShatAppServer/src/view/ChatViewController.java
+++ ShatAppServer/src/view/ChatViewController.java
@@ -1,5 +1,35 @@
 package view;
 
-public class ChatViewController
-{
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import viewmodel.ChatViewModel;
+
+public class ChatViewController {
+    private ViewHandler view;
+    private Region root;
+    private ChatViewModel viewModel;
+
+    @FXML private ListView<String> chatList;
+    @FXML private Label onlineCountLabel;
+    @FXML private Button refreshButton;
+    @FXML private Button settingsButton;
+    @FXML private Button logButton;
+    @FXML private TextField messageTextField;
+    @FXML private Button sendButton;
+
+    public void init(ViewHandler view, ChatViewModel viewModel, Region root) {
+        this.view = view;
+        this.root = root;
+        this.viewModel = viewModel;
+    }
+
+    @FXML public void refreshButtonPressed() {}
+    @FXML public void settingsButtonPressed() {}
+    @FXML public void logButtonPressed() {}
+    @FXML public void sendButtonPressed() {}
+    @FXML public void sendButtonOnEnterPressed() {}
 }
--- ShatAppServer/src/view/SettingsViewController.java
+++ ShatAppServer/src/view/SettingsViewController.java
@@ -1,5 +1,28 @@
 package view;
 
-public class SettingsViewController
-{
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import viewmodel.ChatViewModel;
+
+public class SettingsViewController {
+    private ViewHandler view;
+    private Region root;
+    private ChatViewModel viewModel;
+
+    @FXML private TextField usernameTextField;
+    @FXML private TextField ipTextField;
+    @FXML private TextField portTextField;
+    @FXML private Button applyButton;
+    @FXML private Button cancelButton;
+
+    public void init(ViewHandler view, ChatViewModel viewModel, Region root) {
+        this.view = view;
+        this.root = root;
+        this.viewModel = viewModel;
+    }
+    @FXML public void applyButtonPressed() {}
+    @FXML public void cancelButtonPressed() {}
+
 }
