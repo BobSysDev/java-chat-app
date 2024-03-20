38f0bdf588d5e8346beaf179db2e40b2086c24ef
KVECKO <samo.knieza@gmail.com>
2024-03-20 11:12:33 +0100

    d

--- ShatAppClient/src/mediator/ClientTest.java
+++ ShatAppClient/src/mediator/ClientTest.java
@@ -6,6 +6,6 @@ public class ClientTest
 {
   public static void main(String[] args) throws IOException
   {
-    MessageClient messageClient = new MessageClient("localhost",5678);
+    MessageClient messageClient = new MessageClient("10.154.196.72",5678);
   }
 }
