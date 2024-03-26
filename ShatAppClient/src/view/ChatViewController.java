a9ece564f744ca3455ed0186053e42972b26b72f
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2024-03-26 15:09:33 +0100

    Update ChatViewController.java

--- ShatAppClient/src/view/ChatViewController.java
+++ ShatAppClient/src/view/ChatViewController.java
@@ -16,17 +16,17 @@ public class ChatViewController
 
     @FXML private ListView<String> chatList;
     @FXML private Label onlineCountLabel;
-    @FXML private TextField messageTextField;
-    @FXML private Button sendButton;
-    @FXML private Button settingsButton;
+    // @FXML private TextField messageTextField;
+    //@FXML private Button sendButton;
+    //@FXML private Button settingsButton;
 
     public void init(ViewHandler view, ChatViewModel viewModel, Region root) {
         this.view = view;
         this.root = root;
         this.viewModel = viewModel;
-        this.messageTextField.setVisible(false);
-        this.sendButton.setVisible(false);
-        this.settingsButton.setVisible(false);
+        //this.messageTextField.setVisible(false);
+        //this.sendButton.setVisible(false);
+        //this.settingsButton.setVisible(false);
         this.chatList.setItems(viewModel.getMessages());
         this.onlineCountLabel.textProperty().bind(viewModel.getOnlineCountLabel());
     }
@@ -41,12 +41,12 @@ public class ChatViewController
         viewModel.refresh();
     }
     @FXML public void settingsButtonPressed() {
-//        view.openView("settings");
+        view.openView("settings");
     }
     @FXML public void logButtonPressed() {}
     @FXML public void sendButtonPressed() {
-//        String content = messageTextField.getText();
-
+        String content = messageTextField.getText();
+        viewModel.viewModelSend(content);
     }
     @FXML public void sendButtonOnEnterPressed() {}
 
